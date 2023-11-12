d4a7df020cb885ad3a45e31922f9ec391aa96ff4
jonnysoe <84360198+jonnysoe@users.noreply.github.com>
2023-11-12 18:29:19 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# SVG Annotator
+
+Primitive single-script (`JavaScript`) to annotate name into [SVG](https://www.w3.org/Graphics/SVG) image.
+This can usually be helpful to annotate documents like certificates, name cards, invitation cards, etc.
+
+## Requirements
+
+- [nodejs](https://nodejs.org/en/download)
+
+### Optional
+
+- [git](https://git-scm.com/downloads)
+- [cURL](https://curl.se/download.html) - pre-installed on Windows and MacOS; Linux environment could either install curl (`sudo apt install -y curl`) or use the pre-installed [wget](https://www.gnu.org/software/wget/manual/wget.html) as alternative
+- [yarn](https://classic.yarnpkg.com/lang/en/docs/install) (`npm install --global yarn`)
+
+## Usage
+
+Annotate `SVG` image formats with the names from user input argument or text file (for multiple names to different `SVG` files), based on the given `SVG` template.
+
+Script Arguments:
+- `name` (optional): Intended name for annotation, can be a name or a file, defaults to check if `names.txt` exists for list of names if none was provided.
+- `template` (optional): `SVG` template to be annotated, defaults to check if `template.svg` exists to be annotated.
+- `font` (optional): Font style, as long they are recognized by `inkscape`.
+
+### Reference
+Check out [sample/names.txt](./sample/names.txt) and [sample/template.svg](./sample/template.svg).
+
+### Getting Started
+1. Start with either of the following, then `cd`` into the directory.
+    - git clone
+        ```
+        git clone git@github.com:jonnysoe/svg-annotator.git && cd svg-annotator
+        ```
+    - download the single [svg-annotator.js](./svg-annotator.js) script into a temp directory
+        ```
+        mkdir svg-annotator && curl -L -o svg-annotator.js https://raw.githubusercontent.com/jonnysoe/svg-annotator/main/svg-annotator.js && cd svg-annotator
+        ```
+2. Prepare name list by the name `names.txt` (separated by new lines) in project root directory.
+3. Prepare a template svg file by the name `template.svg`.
+4. Run this command:
+    ```
+    node svg-annotator.svg
+    ```
+   Alternatively, can run the following when [package.json](./package.json) is present
+    ```
+    node .
+    ```
+    ```
+    npm run start
+    ```
+    ```
+    yarn run start
+    ```
+    NOTE: Single file svg-annotator.js will still generate [package.json](./package.json) file.
+
+### TODO
+- Annotate a single `SVG` image with multiple names, might wanna change `names.txt` to `CSV` format where names on the same row separated by comma will appear on the same `SVG` file
+
+## License
+
+Copyright © 2023 Jonny Soe.
+
+Licensed under [the MIT License (MIT)](https://opensource.org/licenses/MIT) or see [LICENSE](./LICENSE.md) file for details.
---
+++ svg-annotator.js
@@ -0,0 +1,290 @@
+#!/usr/bin/env node
+
+const process = require('process');
+const cp = require('child_process');
+const fs = require('fs');
+const path = require('path');
+
+//======================================================================
+// Global variables
+//======================================================================
+const OUT = 'out';
+const INKSCAPE = (process.platform === 'win32') ? 'inkscapecom.com' : 'inkscape';
+const SCALE = 0.92;
+let debug = false;
+
+//======================================================================
+// Helper Routine
+//======================================================================
+const debug_log = (obj) => {
+    if (!!debug) {
+        if ((typeof(obj) === 'object') && !Array.isArray(obj)) {
+            console.log(JSON.stringify(obj, null, 4));
+        } else {
+            console.log(obj);
+        }
+    }
+}
+
+//======================================================================
+// Shell Emulation Routines
+//======================================================================
+const directories = [];
+const pushd = (dir) => {
+    directories.push(process.cwd());
+    process.chdir(dir);
+};
+const popd = () => {
+    let dir = directories.pop();
+    if (dir) {
+        process.chdir(dir);
+    }
+};
+
+// https://stackoverflow.com/a/31104898/19336104
+const termSync = (commandLine) => {
+    debug_log(`> ${commandLine}`);
+    cp.execSync(commandLine, {stdio: 'inherit'});
+};
+
+// Equivalent to `mkdir -p`
+const mkdirSync = (dir) => {
+    const cwd = process.cwd();
+    dir.split(path.sep).forEach((subdir) => {
+        if (!fs.existsSync(subdir)) {
+            fs.mkdirSync(subdir);
+        }
+        process.chdir(subdir);
+    });
+    process.chdir(cwd);
+};
+
+const where = (command) => {
+    try {
+        if (process.platform === 'win32') {
+            return cp.execSync(`where ${command}`, {stdio: 'pipe'});
+        }
+        return cp.execSync(`command -v ${command}`, {stdio: 'pipe'});
+    } catch (err) {
+        // ignore error
+    }
+    return undefined;
+};
+
+const cmdExist = (command) => {
+    return !!where(command);
+};
+
+//======================================================================
+// Package Manager Handler
+//======================================================================
+class Pacman {
+    constructor() {
+        this.name = this.getPacman();
+
+        // https://www.digitalocean.com/community/tutorials/nodejs-npm-yarn-cheatsheet
+        if (this.name === 'yarn') {
+            this.cmdMap = {
+                'install': 'install',
+                'add': 'add'
+            };
+        } else {
+            this.cmdMap = {
+                'install': 'install',
+                'add': 'install'
+            };
+        }
+    }
+
+    getPacman = () => {
+        // Tricky part is there is no way to check if script was invoked by npm or yarn,
+        // so this can still override npm invocation to yarn
+        if (fs.existsSync('yarn.lock') || (!fs.existsSync('package-lock.json') && cmdExist('yarn'))) {
+            return 'yarn';
+        }
+        return 'npm';
+    };
+
+    install = () => {
+        termSync(`${this.name} ${this.cmdMap['install']}`);
+    };
+
+    add = (dependency) => {
+        termSync(`${this.name} ${this.cmdMap['add']} ${dependency}`);
+    };
+}
+
+//======================================================================
+// Script Entry
+//======================================================================
+pushd(__dirname);
+
+// Check dependencies
+if (!cmdExist(INKSCAPE)) {
+    console.log(`"${INKSCAPE}" is not installed!`);
+    console.log('Output will only be in SVG...');
+    // process.exit(1);
+}
+
+// Prepare default package.json, variable cannot use package as name
+const script = path.basename(__filename);
+let project = {
+    name: `${path.basename(script, path.extname(script))}`,
+    version: '1.0.0',
+    description: 'Primitive single-script (JavaScript) to annotate name into SVG image',
+    main: `${script}`,
+    scripts: {
+        start: 'node .',
+    },
+    author: `${process.env.USERNAME}`,
+    license: 'MIT',
+};
+
+// Select package manager
+const pacman = new Pacman();
+
+// Prepare new package.json
+const packageExist = fs.existsSync('package.json');
+if (packageExist) {
+    project = JSON.parse(fs.readFileSync('package.json', 'utf-8'));
+} else {
+    fs.writeFileSync('package.json', JSON.stringify(project, null, 4));
+}
+
+// Make sure dependencies are added, whether its new or started from single file.
+const addDependencies = !project.hasOwnProperty('dependencies');
+if (addDependencies || !project.dependencies.hasOwnProperty('jsdom')) {
+    pacman.add('jsdom');
+}
+if (addDependencies || !project.dependencies.hasOwnProperty('yargs')) {
+    pacman.add('yargs');
+}
+
+//======================================================================
+// Parse args
+//======================================================================
+// Ignore first 2 arguments <node exetubale> and <script>
+const argv = require('yargs')(process.argv.slice(2))
+    .usage('Usage: $0 -n [name] -t [template]')
+    .alias('v', 'version')
+    .example('$0 -n "Misaka Mikoto"', 'Annotates template.svg (default template SVG) with "Misaka Mikoto"')
+    // Name(s)
+    .default('n', 'names.txt')
+    .alias('n', 'name')
+    .nargs('n', 1)
+    .describe('n', 'Load a name or a file')
+    // Template SVG
+    .default('t', 'template.svg')
+    .alias('t', 'template')
+    .nargs('t', 1)
+    .describe('t', 'template SVG file to be annotated')
+    // Font
+    .default('font-style', 'cmmi10')
+    .nargs('t', 1)
+    .describe('font-style', 'Font style for the name(s), need to make sure inkscape recognize this')
+    .default('font-size', 14)
+    .nargs('t', 1)
+    .describe('font-size', 'Font size for the name(s)')
+    .default('font-scale', 0.92)
+    .nargs('t', 1)
+    .describe('font-scale', 'Font scale for the name(s)')
+    .hide('font-scale')
+    // debug
+    .boolean('debug')
+    .alias('d', 'debug')
+    // help
+    .help('h')
+    .alias('h', 'help')
+    .argv;
+
+debug = !!argv.debug;
+const template = argv.template;
+const name = argv.name;
+if (path.extname(name) && !fs.existsSync(name)) {
+    console.log(`${name} does not exist!`);
+    process.exit(2);
+}
+const names = fs.existsSync(name) ?
+    fs.readFileSync('names.txt', 'utf-8').replace('\r', '\n').replace('\n\n', '\n').split('\n') :
+    [name];
+
+debug_log(pacman);
+debug_log(argv);
+
+// Load package.json if it exist originally
+if (packageExist) {
+    pacman.install();
+}
+
+// Install dependencies
+const jsdom = require("jsdom");
+const { command } = require('yargs');
+const { JSDOM } = jsdom;
+
+const annotate = (name, data) => {
+    // Convert file to DOM and get elements for modifications
+    const dom = new JSDOM(data, { contentType: 'image/svg+xml' });
+
+    // Get svg selector
+    let svg = dom.window.document.querySelector('svg');
+    svg = dom.window.document.getElementById(svg.id);
+    const width = svg.getAttribute('width');
+    const height = svg.getAttribute('height');
+    console.log(width);
+    console.log(height);
+
+    // Create text selector
+    // https://stackoverflow.com/a/13229110/19336104
+    const text = dom.window.document.createElement('text', '');
+
+    // Populate text selector
+    {
+        let i = 0;
+        let id = 'text';
+        do {
+            i++;
+            id = `text${i}`;
+        } while (!!dom.window.document.getElementById(`${id}`));
+        text.setAttribute('id', id);
+        text.setAttribute('xml:space', 'preserve');
+        text.setAttribute('style', `font-style:normal;font-weight:normal;font-size:${argv.fontSize}px;line-height:1.25;font-family:${argv.fontStyle};letter-spacing:0px;word-spacing:4px;fill:#000000;fill-opacity:1;stroke:none;stroke-width:0.75094575`);
+        text.setAttribute('y', '214.18192');
+
+        // Lastly approximate x position
+        const x = (Number(width) - (name.length * argv.fontSize * argv.fontScale)) / 2;
+        text.setAttribute('x', `${x}`);
+    }
+
+    // Update Text Content
+    text.textContent = name;
+
+    svg.appendChild(text);
+
+    // Modify filename
+    const file = name.replace(/\s/g, '');
+    const svg_file = file + '.svg';
+    svg.setAttribute('sodipodi:docname', svg_file);
+
+    // Write to file
+    // Strangely, xmlns attribute will be automatically added after appendChild, remove it before writing to file.
+    console.log(`Anotating "${name}" to ${template}`);
+    fs.writeFileSync(path.join(OUT, svg_file), dom.serialize().replace(' xmlns=""', ''), 'utf8');
+
+    // Use inkscape to convert to PNG
+    if (cmdExist(INKSCAPE)) {
+        termSync(`${INKSCAPE} ${path.join(OUT, svg_file)} --export-png=${path.join(OUT, file + '.png')}`);
+    }
+};
+
+// Make output directory
+mkdirSync(OUT);
+
+// Anotate names into SVG
+const data = fs.readFileSync(template, 'utf8');
+names.forEach((name) => {
+    annotate(name, data);
+});
+
+popd();
+
+process.exit(0);
